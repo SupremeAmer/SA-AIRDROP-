2c400c42b624085f9449e11a50dbabf914656ebb
SupremeAmer <emmyplumbingservice@gmail.com>
2025-01-03 08:21:37 +0100

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,19 +1,1 @@
-/ Fetch user $SA balance data from database or API
-fetch('/api/users')
-  .then(response => response.json())
-  .then(data => {
-    // Sort users by $SA balance in descending order
-    data.sort((a, b) => b.saBalance - a.saBalance);
 
-    // Populate leaderboard table
-    const leaderboardData = document.getElementById('leaderboard-data');
-    data.forEach((user, index) => {
-      const row = document.createElement('tr');
-      row.innerHTML = `
-        <td>${index + 1}</td>
-        <td>${user.username}</td>
-        <td>${user.saBalance}</td>
-      `;
-      leaderboardData.appendChild(row);
-    });
-  });
