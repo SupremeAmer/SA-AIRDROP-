636f94ba78dc2b2a8ae10cd8783dc92bc13ebab6
SupremeAmer <emmyplumbingservice@gmail.com>
2025-01-04 02:05:50 +0100

    Update and rename index.js to script.py

---
+++ script.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, request, jsonify
+import telegram
+
+app = Flask(*name*)
+
+bot_token = '8040618454:AAFpwP49XKefk2z2v8ZX6XiaOfKbMs4chdg'
+bot = telegram.Bot(token=bot_token)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+if request.method == 'POST':
+update = telegram.Update.de_json(request.get_json(), bot)
+chat_id = (7053015856)
+bot.sendMessage(chat_id, 'Hello! You participated!')
+return 'OK'
+else:
+return 'Participate button clicked!'
+
+if *name* == '*main*':
+app.run()
